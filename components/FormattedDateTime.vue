7678ebd6ee9658baa859a130713b1e3147b78ffc
Tigran <harutyunyan.tigran1975@gmail.com>
2025-02-04 11:51:50 +0400

    created SummaryItem and FormatteDateTime components

---
+++ components/FormattedDateTime.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { cn, formatDateTime } from "@/lib/utils";
+
+interface FormattedDateTimeProps {
+  date: string;
+  className?: string;
+}
+
+defineProps<FormattedDateTimeProps>();
+</script>
+
+<template>
+  <p :class="cn('body-1 text-light-200', className)">
+    {{ formatDateTime(date) }}
+  </p>
+</template>
---
+++ components/SummaryItem.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { Separator } from "@/components/ui/separator";
+import { convertFileSize } from "@/lib/utils";
+
+interface SummaryItemPros {
+  summary: {
+    url: string;
+    title: string;
+    icon: string;
+    size: number;
+    latestDate: string;
+  };
+}
+
+defineProps<SummaryItemPros>();
+</script>
+
+<template>
+  <NuxtLink
+    :to="summary.url"
+    :key="summary.title"
+    class="relative mt-6 rounded-[20px] bg-white p-5 transition-all hover:scale-105"
+  >
+    <div class="space-y-4">
+      <div class="flex justify-between gap-3">
+        <img
+          :src="summary.icon"
+          width="100"
+          height="100"
+          alt="uploaded image"
+          class="absolute -left-3 top-[-25px] z-10 w-[190px] object-contain"
+        />
+        <h4 class="h4 relative z-20 w-full text-right">
+          {{ convertFileSize(summary.size) || 0 }}
+        </h4>
+      </div>
+
+      <h5 class="h5 relative z-20 text-center">
+        {{ summary.title }}
+      </h5>
+      <Separator class="bg-light-400" />
+      <FormattedDateTime :date="summary.latestDate" className="text-center" />
+    </div>
+  </NuxtLink>
+</template>
