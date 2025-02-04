80f722905f909b83a1df1363aa817f251487ce99
Tigran <harutyunyan.tigran1975@gmail.com>
2025-02-04 12:19:20 +0400

    added constants

---
+++ constants.ts
@@ -0,0 +1,12 @@
+export const pathsEnum = {
+    SIGN_IN: '/sign-in',
+    SIGN_UP: '/sign-up'
+} as const;
+
+
+export const avatarPlaceholderUrl =
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg";
+
+export const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB
+
+export const AUTH_COOKIE = "cwa-cloud-next-session";
