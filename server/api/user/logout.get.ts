fb4cd123701af47ab1700ae693ccd43e9fe84be6
Tigran <harutyunyan.tigran1975@gmail.com>
2025-02-04 13:50:08 +0400

    just delete cookie for now

--- server/api/user/logout.get.ts
+++ server/api/user/logout.get.ts
@@ -1,13 +1,8 @@
 import { AUTH_COOKIE } from "@/constants";
-import { createAdminClient } from "@/lib/appwrite";
 
 export default defineEventHandler(async (event) => {
     try {
         deleteCookie(event, AUTH_COOKIE);
-
-        const { account } = await createAdminClient();
-        await account.deleteSession('current');
-        await account.deleteSession("appwrite-session");
 
         return { success: true };
 
