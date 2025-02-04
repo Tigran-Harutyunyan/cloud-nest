fbd84022a9e7cd3fbe2a2df5b1416c66c41d9878
Tigran <harutyunyan.tigran1975@gmail.com>
2025-02-04 11:54:16 +0400

    app file

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Toaster } from "vue-sonner";
+useSeoMeta({
+  title: "Cloud nest",
+  description: "Cloud-nest The only storage solution you need.",
+});
+</script>
+<template>
+  <NuxtLoadingIndicator color="#EA6365" />
+  <NuxtPage />
+  <Toaster
+    class="pointer-events-auto"
+    position="bottom-right"
+    :rich-colors="true"
+    :close-button="true"
+  />
+</template>
